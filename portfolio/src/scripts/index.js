c6699529f08afb5194693dcb8be831a5c05b6a3a
Eldrick <eldrick@google.com>
2020-05-29 14:21:02 -0400

    added portfolio (separate repo is on profile)

---
+++ portfolio/src/scripts/index.js
@@ -0,0 +1,12 @@
+import '../styles/index.scss';
+
+console.log('webpack starterkit');
+
+
+var carousel = new Flickity(".carousel", {
+    prevNextButtons: false,
+    pageDots: false,
+    freeScroll: true,
+    contain: true,
+    draggable: true,
+});
